b05e9fec0d9f8bcdd03fec38ff24f80e80a322f5
choihoseong <to6044@naver.com>
2026-03-03 16:58:45 +0900

    test17

--- 01-infra/user_data.sh
+++ 01-infra/user_data.sh
@@ -2,7 +2,7 @@
 
 # Squid Proxy 설치
 apt update -y
-apt install squid -y              
+apt install squid jq -y              
 sed -i 's/http_access deny all/http_access allow all/g' /etc/squid/squid.conf
 systemctl restart squid
 
@@ -12,14 +12,21 @@ GITHUB_PAT="${github_pat}"
 RUNNER_NAME="bastion-org-runner-$(hostname)"
 
 # GITHUB API를 통해 조직 레벨 등록 토큰 가져오기
+REG_TOKEN=$(curl -s -X POST \
   -H "Authorization: token $GITHUB_PAT" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/orgs/$GITHUB_ORG/actions/runners/registration-token | jq -r .token)
-
+  
 if [ "$REG_TOKEN" == "null" ] || [ -z "$REG_TOKEN" ]; then
     echo "Error: Failed to get Org registration token. Check 'admin:org' permission."
     exit 1
 fi
+
+
+REG_TOKEN=$(curl -s -X POST \
+  -H "Authorization: token $GITHUB_PAT" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/orgs/$GITHUB_ORG/actions/runners/registration-token | jq -r .token)
 
 # Runner 설치 (버전 다운로드 로직 동일)
 mkdir -p /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner
